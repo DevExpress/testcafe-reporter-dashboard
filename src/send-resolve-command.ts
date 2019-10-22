e45e8c7ba18ce4d72f3db33fd56356c617217bef
aleks-pro <prokhor.aleks@gmail.com>
2019-10-22 18:56:23 +0300

    use fetch instead of http, dashboard location should be set via enviromente variable

--- src/send-resolve-command.ts
+++ src/send-resolve-command.ts
@@ -1,35 +1,40 @@
-const http = require('http');
+const http  = require('http');
+const fetch = require('isomorphic-fetch');
 
-export default function sendResolveCommand (id, commandType, payload) {
-    const AUTHORIZATION_TOKEN = process.env.TESTCAFE_DASHBOARD_AUTHORIZATION_TOKEN;
+const AUTHORIZATION_TOKEN = process.env.TESTCAFE_DASHBOARD_AUTHORIZATION_TOKEN;
+let   DASHBOARD_LOCATION  = process.env.TESTCAFE_DASHBOARD_URL;
 
-    if (!AUTHORIZATION_TOKEN) {
-        console.error('\'TESTCAFE_DASHBOARD_AUTHORIZATION_TOKEN\' is not defined');
+if (!DASHBOARD_LOCATION) {
+    DASHBOARD_LOCATION = 'http://localhost:3000';
 
+    console.warn(`The 'TESTCAFE_DASHBOARD_URL' enviromente variable is not defined. The ${DASHBOARD_LOCATION} url will be used by default.`)
+}
+
+if (!AUTHORIZATION_TOKEN)
+    console.error('\'TESTCAFE_DASHBOARD_AUTHORIZATION_TOKEN\' is not defined');
+
+export default async function sendResolveCommand (id, commandType, payload) {
+    if (!AUTHORIZATION_TOKEN)
         return;
-    }
 
     return new Promise(async (resolve) => {
-        const request = new http.ClientRequest({
-            hostname: 'localhost',
-            port:     3000,
-            path:     '/api/commands/',
-            method:   'POST',
-            headers:  {
-                'Content-Type': 'application/json'
-            }
-        }, cb => {
-            console.log(`${commandType}: ${cb.statusCode} ${cb.statusMessage}`);
+        fetch(`${DASHBOARD_LOCATION}/api/commands/`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                type:          commandType,
+                aggregateId:   id,
+                aggregateName: 'Report',
+
+                payload: { ownerId: AUTHORIZATION_TOKEN, ...payload },
+            })
+        })
+        .then(function(response) {
+            console.log(`${commandType}: ${response.status} ${response.statusText}`);
+
+            resolve()
         });
-
-        request.end(JSON.stringify({
-            type:          commandType,
-            aggregateId:   id,
-            aggregateName: 'Report',
-
-            payload: { ownerId: AUTHORIZATION_TOKEN, ...payload },
-        }));
-
-        setTimeout(resolve, 100);
-    })
+    });
 }
