735dbbba017dff13a32ff769205e1b9271b02f3d
Sergey Shurygin <sergey.shruygin@devexpress.com>
2020-03-11 15:39:09 +0300

    try to write decorator to produce a json instead of html

--- src/error-decorator.ts
+++ src/error-decorator.ts
@@ -1,36 +1,34 @@
 const raw = (str: string) => str;
+const syntax = (type: string) => (str: string) => `{ text: "${str}", type: "${type}}", `;
+const element = (name: string) => (str: string) => `${name}: "${str}", `;
+const braced = (str: string) => `{${str}}`
 
 export function errorDecorator() {
     return {
-        'span user-agent': raw,
+        'span user-agent': element('user-agent'),
 
-        'span subtitle': raw,
-        'div message': (str: string) => `<code class="message">${str}</code>`,
+        'span subtitle': element('subtitle'),
+        'div message': (str: string) => `{ message: ${str}}`,
 
-        'div screenshot-info': (str: string) => `<div class="screenshot-info">${str}</div>`,
-        'a screenshot-path': (str: string) => {
-            return `<a href="${str}" class="thumbnail">
-                                <div class="overlay"></div>
-                                <div class="zoom-icon"></div>
-                            </a>`;
-        },
+        'div screenshot-info': element('screenshot-info'),
+        'a screenshot-path': element('screenshot-path'),
 
-        'code': (str: string) => `<code class="language-javascript">${str}</code>`,
+        'code': element('code'),
 
-        'span syntax-string': raw,
-        'span syntax-punctuator': raw,
-        'span syntax-keyword': raw,
-        'span syntax-number': raw,
-        'span syntax-regex': raw,
-        'span syntax-comment': raw,
-        'span syntax-invalid': raw,
+        'span syntax-string': syntax('string'),
+        'span syntax-punctuator': syntax('string'),
+        'span syntax-keyword': syntax('string'),
+        'span syntax-number': syntax('string'),
+        'span syntax-regex': syntax('string'),
+        'span syntax-comment': syntax('string'),
+        'span syntax-invalid': syntax('string'),
 
-        'div code-frame': (str: string) => `<div class="multiline_editor"><div class="go-to-code-btn language-javascript">${str}</div></div>`,
-        'div code-line': (str: string) => `${str}\n`,
-        'div code-line-last': raw,
-        'div code-line-num': (str: string) => `   ${str} | `,
-        'div code-line-num-base': (str: string) => ` > ${str} | `,
-        'div code-line-src': raw,
+        'div code-frame': (str: string) => `code-frame: [${str}]`,
+        'div code-line': braced,
+        'div code-line-last': braced,
+        'div code-line-num': str => `${element('code-line-num')(str)}`,
+        'div code-line-num-base': str => `${element('code-line-num')(str)}, base=true`,
+        'div code-line-src': (str: string) => `code: [${str}]}, `,
 
         'div stack': (str: string) => '\n\n' + str,
         'div stack-line': (str: string) => `${str}\n`,
