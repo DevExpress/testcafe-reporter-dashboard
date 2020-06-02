584dfe7e504c5f41d9e1cdb70773e29634c0f639
aleks-pro <prokhor.aleks@gmail.com>
2020-06-02 12:07:34 +0300

    Log reporter methods calls and wrap with try catch

---
+++ src/assign-reporter-methods.ts
@@ -0,0 +1,20 @@
+import { ReporterMethods, ReporterPluginObject } from './types/testcafe';
+import logger from './logger';
+import { ENABLE_LOG } from './env-variables';
+
+export default function assignReporterMethods (reporterMethods: ReporterMethods, reporterObject: ReporterPluginObject) {
+    for (const methodName of Object.keys(reporterMethods)) {
+        reporterObject[methodName] = async function (...args) {
+            if (ENABLE_LOG)
+                logger.log(`${methodName}: ${JSON.stringify(args)}`);
+
+            try {
+                await reporterMethods[methodName].apply(this, args);
+            }
+            catch (e) {
+                //logger.error(e);
+                throw e;
+            }
+        };
+    }
+}
