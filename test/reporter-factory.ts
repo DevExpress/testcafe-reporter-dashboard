6a9ba1d172b4d5c23660eb25f6a14eb2041ff377
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2020-06-18 03:38:52 +0300

    uncomment test

--- test/reporter-factory.ts
+++ test/reporter-factory.ts
@@ -4,7 +4,7 @@ import assert from 'assert';
 import { reportTestActionDoneCalls } from './data/report-test-action-done-calls';
 import { testDoneInfo, twoErrorsTestActionDone, thirdPartyTestDone } from './data/';
 import { buildReporterPlugin, TestRunErrorFormattableAdapter } from 'testcafe/lib/embedding-utils';
-// import { MAX_BUILD_ID_LENGTH } from '../src';
+import { MAX_BUILD_ID_LENGTH } from '../src';
 
 const TESTCAFE_DASHBOARD_URL = 'http://localhost';
 const TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN = 'authentication_token';
@@ -41,37 +41,37 @@ describe('reportTaskStart', () => {
         mock.stop('../lib/env-variables');
     });
 
-    // it('Show reporter long build id message', async () => {
-    //     const longBuildId = 'test_build_id/123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890';
+    it('Show reporter long build id message', async () => {
+        const longBuildId = 'test_build_id/123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890';
 
-    //     const projectId = 'mock_project_id';
-    //     const reportId = 'mock_report_id';
+        const projectId = 'mock_project_id';
+        const reportId = 'mock_report_id';
 
-    //     mock('uuid', ()=> {
-    //         return reportId;
-    //     });
+        mock('uuid', ()=> {
+            return reportId;
+        });
 
-    //     mock('isomorphic-fetch', () => {
-    //         return Promise.resolve({ ok: true, status: 200, statusText: 'OK' });
-    //     });
+        mock('isomorphic-fetch', () => {
+            return Promise.resolve({ ok: true, status: 200, statusText: 'OK' });
+        });
 
-    //     mock('jsonwebtoken', {
-    //         decode: () => ({ projectId })
-    //     });
+        mock('jsonwebtoken', {
+            decode: () => ({ projectId })
+        });
 
-    //     mock('../lib/env-variables', {
-    //         TESTCAFE_DASHBOARD_URL,
-    //         TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN,
+        mock('../lib/env-variables', {
+            TESTCAFE_DASHBOARD_URL,
+            TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN,
 
-    //         TESTCAFE_DASHBOARD_BUILD_ID: longBuildId
-    //     });
+            TESTCAFE_DASHBOARD_BUILD_ID: longBuildId
+        });
 
-    //     await assertReporterMessage(`Build ID cannot be longer than ${MAX_BUILD_ID_LENGTH} symbols. Build ID: ${longBuildId}.`);
+        await assertReporterMessage(`Build ID cannot be longer than ${MAX_BUILD_ID_LENGTH} symbols. Build ID: ${longBuildId}.`);
 
-    //     mock.stop('uuid');
-    //     mock.stop('jsonwebtoken');
-    //     mock.stop('isomorphic-fetch');
-    // });
+        mock.stop('uuid');
+        mock.stop('jsonwebtoken');
+        mock.stop('isomorphic-fetch');
+    });
 
     it('Show reporter URL message', async () => {
         const projectId = 'mock_project_id';
