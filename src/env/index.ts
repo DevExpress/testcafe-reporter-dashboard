576d26407904c6a48e2b9c9c46cfdb7ec242bf8c
arubtsov <arseniy.rubtsov@gmail.com>
2021-02-17 16:14:54 +0300

    refactor env variables export

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,10 +1,12 @@
 import { parseBooleanVariable } from './utils';
 
-export const ENABLE_LOG             = parseBooleanVariable(process.env.ENABLE_LOG);
-export const TESTCAFE_DASHBOARD_URL = process.env.TESTCAFE_DASHBOARD_URL || 'https://ddev.testcafe.io';
+const { env } = process;
 
-export const NO_SCREENSHOT_UPLOAD = parseBooleanVariable(process.env.NO_SCREENSHOT_UPLOAD);
-export const NO_VIDEO_UPLOAD      = parseBooleanVariable(process.env.NO_VIDEO_UPLOAD);
+export const ENABLE_LOG             = parseBooleanVariable(env.ENABLE_LOG);
+export const TESTCAFE_DASHBOARD_URL = env.TESTCAFE_DASHBOARD_URL || 'https://ddev.testcafe.io';
 
-export const TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN = process.env.TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN;
-export const TESTCAFE_DASHBOARD_BUILD_ID: string | undefined = process.env.TESTCAFE_DASHBOARD_BUILD_ID;
+export const NO_SCREENSHOT_UPLOAD = parseBooleanVariable(env.NO_SCREENSHOT_UPLOAD);
+export const NO_VIDEO_UPLOAD      = parseBooleanVariable(env.NO_VIDEO_UPLOAD);
+
+export const TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN = env.TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN;
+export const TESTCAFE_DASHBOARD_BUILD_ID: string | undefined = env.TESTCAFE_DASHBOARD_BUILD_ID;
