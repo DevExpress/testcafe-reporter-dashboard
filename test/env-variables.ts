01c4c5913faac6ba429616eb016999955a24c61c
arubtsov <arseniy.rubtsov@gmail.com>
2021-02-25 11:46:04 +0300

    remove unnecessary describe from env-variables.ts test

--- test/env-variables.ts
+++ test/env-variables.ts
@@ -2,26 +2,24 @@ import assert from 'assert';
 import { NO_SCREENSHOT_UPLOAD, NO_VIDEO_UPLOAD } from '../src/env';
 
 describe('enviroment variables', () => {
-    describe('enviroment variables', () => {
-        it('Screenshots and videos upload should be enabled by default', () => {
-            assert.equal(NO_SCREENSHOT_UPLOAD, false);
-            assert.equal(NO_VIDEO_UPLOAD, false);
-        });
+    it('Screenshots and videos upload should be enabled by default', () => {
+        assert.equal(NO_SCREENSHOT_UPLOAD, false);
+        assert.equal(NO_VIDEO_UPLOAD, false);
+    });
 
-        it('Screenshots and videos upload should be enabled if value is "false"', () => {
-            process.env.NO_SCREENSHOT_UPLOAD = 'false';
-            process.env.NO_VIDEO_UPLOAD      = 'false';
+    it('Screenshots and videos upload should be enabled if value is "false"', () => {
+        process.env.NO_SCREENSHOT_UPLOAD = 'false';
+        process.env.NO_VIDEO_UPLOAD      = 'false';
 
-            assert.equal(NO_SCREENSHOT_UPLOAD, false);
-            assert.equal(NO_VIDEO_UPLOAD, false);
-        });
+        assert.equal(NO_SCREENSHOT_UPLOAD, false);
+        assert.equal(NO_VIDEO_UPLOAD, false);
+    });
 
-        it('Screenshots and videos upload should be enabled if value is "0"', () => {
-            process.env.NO_SCREENSHOT_UPLOAD = '0';
-            process.env.NO_VIDEO_UPLOAD      = '0';
+    it('Screenshots and videos upload should be enabled if value is "0"', () => {
+        process.env.NO_SCREENSHOT_UPLOAD = '0';
+        process.env.NO_VIDEO_UPLOAD      = '0';
 
-            assert.equal(NO_SCREENSHOT_UPLOAD, false);
-            assert.equal(NO_VIDEO_UPLOAD, false);
-        });
+        assert.equal(NO_SCREENSHOT_UPLOAD, false);
+        assert.equal(NO_VIDEO_UPLOAD, false);
     });
 });
