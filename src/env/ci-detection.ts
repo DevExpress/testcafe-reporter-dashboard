ac6adce382085cb7a1854eeadd5b17c6e3e00f89
arubtsov <arseniy.rubtsov@gmail.com>
2021-03-01 15:45:21 +0300

    remove unused constants from ci-detection.ts

--- src/env/ci-detection.ts
+++ src/env/ci-detection.ts
@@ -2,13 +2,4 @@ import { parseBooleanVariable } from './utils';
 
 const { env } = process;
 
-export const isCircleCi = parseBooleanVariable(env.CIRCLECI);
-export const isTravis = parseBooleanVariable(env.TRAVIS);
-export const isJenkins = parseBooleanVariable(
-    env.JENKINS_HOME || env.JENKINS_URL ||
-    env.HUDSON_URL || env.HUDSON_HOME
-);
-export const isGitLab = parseBooleanVariable(env.GITLAB_CI);
 export const isGithubActions = parseBooleanVariable(env.GITHUB_ACTIONS);
-export const isAppveyor = parseBooleanVariable(env.APPVEYOR);
-export const isTeamcity = parseBooleanVariable(env.TEAMCITY_VERSION);
