b982c4d9113d351611012c33ef6da84c41e79f9b
arubtsov <arseniy.rubtsov@gmail.com>
2021-03-01 15:47:41 +0300

    remove getAuthorByCommit from env/utils/ts

--- src/env/utils.ts
+++ src/env/utils.ts
@@ -1,16 +1,3 @@
-import { promisify } from 'util';
-import { exec } from 'child_process';
-
-const promiseExec = promisify(exec);
-
 export function parseBooleanVariable (value: string): boolean {
     return value === 'false' || value === '0' ? false : !!value;
-}
-
-export async function getAuthorByCommit (
-    commitSha: string
-): Promise<string> {
-    const { stdout, stderr } = await promiseExec(`git log -1 ${commitSha} --pretty="%aN"`);
-
-    return stderr ? '' : stdout;
 }
