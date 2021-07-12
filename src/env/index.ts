f552daa2a03b01545437696dd8a17cfc255b20b7
aleks-pro <prokhor.aleks@gmail.com>
2021-07-12 11:40:52 +0300

    TESTCAFE_DASHBOARD_URL changed to https://dashboard.testcafe.io

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,7 @@ import { getCIInfo } from './get-ci-info';
 const { env } = process;
 
 export const ENABLE_LOG             = parseBooleanVariable(env.ENABLE_LOG);
-export const TESTCAFE_DASHBOARD_URL = env.TESTCAFE_DASHBOARD_URL || 'https://ddev.testcafe.io';
+export const TESTCAFE_DASHBOARD_URL = env.TESTCAFE_DASHBOARD_URL || 'https://dashboard.testcafe.io';
 
 export const NO_SCREENSHOT_UPLOAD = parseBooleanVariable(env.NO_SCREENSHOT_UPLOAD);
 export const NO_VIDEO_UPLOAD      = parseBooleanVariable(env.NO_VIDEO_UPLOAD);
