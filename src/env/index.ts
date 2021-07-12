dcbec004cf080789d92272d4051cce5e32c56e3b
aleks-pro <prokhor.aleks@gmail.com>
2021-07-12 12:09:52 +0300

    Reporter version changed to 0.2.0 (#56)
    * set RESPONSE_TIMEOUT to 2 min
    
    * TESTCAFE_DASHBOARD_URL changed to https://dashboard.testcafe.io
    
    * reporter version updated to 0.2.0, testcafe version bumped to 1.15.0

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,7 @@ import { getCIInfo } from './get-ci-info';
 const { env } = process;
 
 export const ENABLE_LOG             = parseBooleanVariable(env.ENABLE_LOG);
-export const TESTCAFE_DASHBOARD_URL = env.TESTCAFE_DASHBOARD_URL || 'https://ddev.testcafe.io';
+export const TESTCAFE_DASHBOARD_URL = env.TESTCAFE_DASHBOARD_URL || 'https://dashboard.testcafe.io';
 
 export const NO_SCREENSHOT_UPLOAD = parseBooleanVariable(env.NO_SCREENSHOT_UPLOAD);
 export const NO_VIDEO_UPLOAD      = parseBooleanVariable(env.NO_VIDEO_UPLOAD);
@@ -13,6 +13,6 @@ export const TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN = env.TESTCAFE_DASHBOARD_AU
 export const TESTCAFE_DASHBOARD_BUILD_ID: string | undefined = env.TESTCAFE_DASHBOARD_BUILD_ID;
 
 export const REQUEST_RETRY_COUNT = parseNumber(env.TESTCAFE_DASHBOARD_REQUEST_RETRY_COUNT) || 20;
-export const RESPONSE_TIMEOUT    = parseNumber(env.TESTCAFE_DASHBOARD_RESPONSE_TIMEOUT) || 40 * 1000; // CloudFront response timeout + 10 sec
+export const RESPONSE_TIMEOUT    = parseNumber(env.TESTCAFE_DASHBOARD_RESPONSE_TIMEOUT) || 60 * 2 * 1000;
 
 export const CI_INFO = getCIInfo();
