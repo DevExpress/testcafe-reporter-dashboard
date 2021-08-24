a860f7aef80716ddb630bf414b84f57631e97577
aleks-pro <prokhor.aleks@gmail.com>
2021-08-24 15:28:56 +0300

    Get CI info from envs if GitHub action trigger is not pull request (#60)
    * Get CI info from envs if GitHub action trigger is not pull request
    
    * unit tests fixed

--- src/env/github-actions.ts
+++ src/env/github-actions.ts
@@ -2,8 +2,7 @@ import { readFileSync as fsReadFileSync } from 'fs';
 import { CIInfo } from '../types/task-start-args';
 import reporterLogger from '../logger';
 import { createGithubInfoError } from '../texts';
-
-const { env } = process;
+import { getEnvVariable } from './utils';
 
 interface EventInfo {
     'pull_request': {
@@ -21,25 +20,28 @@ export function getGithubActionsInfo (
     readFileSync: typeof fsReadFileSync,
     logger: typeof reporterLogger
 ): CIInfo {
-    let event: EventInfo = {
-        'pull_request': {
-            head: { sha: '', ref: '' },
-            user: { login: '' }
-        }
-    };
+    let event;
 
     try {
-        const rawEvent = readFileSync(env.GITHUB_EVENT_PATH as string, 'utf8');
+        const rawEvent = readFileSync(process.env.GITHUB_EVENT_PATH as string, 'utf8');
 
         event = JSON.parse(rawEvent) as EventInfo;
+
+        if (event && event.pull_request) {
+            return {
+                commitSHA:  event.pull_request.head.sha,
+                author:     event.pull_request.user.login,
+                branchName: event.pull_request.head.ref
+            };
+        }
     }
     catch (error) {
         logger.error(createGithubInfoError(error.toString()));
     }
 
     return {
-        commitSHA:  event.pull_request.head.sha,
-        author:     event.pull_request.user.login,
-        branchName: event.pull_request.head.ref
+        commitSHA:  getEnvVariable('GITHUB_SHA'),
+        author:     getEnvVariable('GITHUB_ACTOR'),
+        branchName: getEnvVariable('GITHUB_REF')
     };
 }
