19ec607efbc75491671b43f37c82a4dc795ba219
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2021-10-07 14:10:32 +0300

    add import

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,6 @@ export * from './task-start-args';
 export * from './test-done-args';
 export * from './test-start-args';
 export * from './task-done-args';
+export * from './report-warnings-args';
 export * from './common';
 export * from './testcafe';
