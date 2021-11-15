e5626ad2f491076584944caef555200886eebb90
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2021-11-15 18:21:47 +0300

    text changed

--- src/texts.ts
+++ src/texts.ts
@@ -19,7 +19,7 @@ export const createTestUploadError = (uploadId: string, testName: string): strin
    `Failed to upload a test log. Upload ID: ${uploadId}, test name: ${testName}.`;
 
 export const createWarningUploadError = (uploadId: string, uploadEntityId: string): string =>
-   `Failed to upload warnings. Upload ID: ${uploadId}, upload entity id: ${uploadEntityId}.`;
+   `Failed to upload warning data. Upload ID: ${uploadId}, upload entity id: ${uploadEntityId}.`;
 
 export const createGetUploadInfoError = (uploadEntityId: string, response: string): string =>
    `Cannot get an upload URL. Upload entity ID: ${uploadEntityId}. Response: ${response}`;
