c115545e7669e354d73ee67c4ac2b74861fe6631
miherlosev <mlosev86@gmail.com>
2022-04-22 16:03:57 +0400

    rename environment variables

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,13 +3,13 @@ import { getCIInfo } from './get-ci-info';
 
 const { env } = process;
 
-export const ENABLE_LOG             = parseBooleanVariable(env.ENABLE_LOG);
+export const ENABLE_LOG             = parseBooleanVariable(env.TESTCAFE_DASHBOARD_ENABLE_LOG);
 export const TESTCAFE_DASHBOARD_URL = env.TESTCAFE_DASHBOARD_URL || 'https://dashboard.testcafe.io';
 
-export const NO_SCREENSHOT_UPLOAD = parseBooleanVariable(env.NO_SCREENSHOT_UPLOAD);
-export const NO_VIDEO_UPLOAD      = parseBooleanVariable(env.NO_VIDEO_UPLOAD);
+export const NO_SCREENSHOT_UPLOAD = parseBooleanVariable(env.TESTCAFE_DASHBOARD_NO_SCREENSHOT_UPLOAD);
+export const NO_VIDEO_UPLOAD      = parseBooleanVariable(env.TESTCAFE_DASHBOARD_NO_VIDEO_UPLOAD);
 
-export const TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN = env.TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN;
+export const TESTCAFE_DASHBOARD_AUTHENTICATION_TOKEN = env.TESTCAFE_DASHBOARD_TOKEN;
 export const TESTCAFE_DASHBOARD_BUILD_ID: string | undefined = env.TESTCAFE_DASHBOARD_BUILD_ID;
 
 export const REQUEST_RETRY_COUNT = parseNumber(env.TESTCAFE_DASHBOARD_REQUEST_RETRY_COUNT) || 20;
