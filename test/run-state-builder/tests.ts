34af504cfe3c0443d583bf27552f766c83e9575e
aleks-pro <prokhor.aleks@gmail.com>
2022-11-07 12:26:43 +0400

    Run state builder class added

---
+++ test/run-state-builder/tests.ts
@@ -0,0 +1,46 @@
+import assert from 'assert';
+import { RunStateBuilder } from '../../src/run-state-builder';
+import { EXPECTED_TASK_STARTED_RUN_STATE_DATA, EXPECTED_TEST_STARTED_RUN_STATE_DATA, REPORT_TASK_START_ARGS_DATA, REPORT_TEST_START_ARGS_DATA } from './data';
+
+describe('Run state builder tests', () => {
+    it('reportTaskStarted', async () => {
+        // arrange
+        const runStateBuilder = new RunStateBuilder('run_1');
+
+        // act
+        runStateBuilder.reportTaskStarted(REPORT_TASK_START_ARGS_DATA);
+
+        const actualState = runStateBuilder.getRunState();
+
+        // assert
+        assert.deepStrictEqual(actualState, EXPECTED_TASK_STARTED_RUN_STATE_DATA);
+    });
+
+    it.only('reportTestStarted', async () => {
+        // arrange
+        const runStateBuilder = new RunStateBuilder('run_1');
+
+        // act
+        runStateBuilder.reportTaskStarted(REPORT_TASK_START_ARGS_DATA);
+        runStateBuilder.reportTestStarted(REPORT_TEST_START_ARGS_DATA);
+
+        const actualState = runStateBuilder.getRunState();
+
+        // assert
+        assert.deepStrictEqual(actualState, EXPECTED_TEST_STARTED_RUN_STATE_DATA);
+    });
+
+    it('reportTestDone', async () => {
+        // arrange
+        const runStateBuilder = new RunStateBuilder('run_1');
+
+        // act
+        runStateBuilder.reportTaskStarted(REPORT_TASK_START_ARGS_DATA);
+        runStateBuilder.reportTestStarted(REPORT_TEST_START_ARGS_DATA);
+
+        const actualState = runStateBuilder.getRunState();
+
+        // assert
+        assert.deepStrictEqual(actualState, EXPECTED_TEST_STARTED_RUN_STATE_DATA);
+    });
+});
