e1a8736baca25a7a88bf95f83850eb3b6e5d372e
aleks-pro <prokhor.aleks@gmail.com>
2022-11-10 12:34:08 +0400

    Run state uploader class added

---
+++ src/run-state-uploader.ts
@@ -0,0 +1,57 @@
+import { RunState } from './types/run';
+import { UploadInfo } from './types/internal';
+
+export const RUN_STATE_UPLOAD_PERIOD = 10000;
+
+export interface Uploader {
+    getUploadInfo (uploadEntityId: string): Promise<UploadInfo>;
+    upload (uploadUrl: string, uploadEntity: Buffer): Promise<void>;
+}
+
+export interface RunStateProvider {
+    getRunState(): Partial<RunState>;
+}
+
+function skipEmptyValues (key, value) {
+    return value instanceof RegExp ? value.toString() : value;
+}
+
+export class RunStateUploader {
+    private _runStateProvider: RunStateProvider;
+    private _uploader: Uploader;
+    private _intervalId: NodeJS.Timeout;
+    private _uploadInfo: UploadInfo;
+    private _runStateUploadPeriod: number;
+
+    async _uploadRunState () {
+        const runState = this._runStateProvider.getRunState();
+
+        const buffer = Buffer.from(JSON.stringify(runState, skipEmptyValues));
+
+        this._uploader.upload(this._uploadInfo.uploadUrl, buffer);
+    }
+
+    constructor (uploader: Uploader, runStateProvider: RunStateProvider, runStateUploadPeriod = RUN_STATE_UPLOAD_PERIOD) {
+        this._uploader = uploader;
+        this._runStateProvider = runStateProvider;
+        this._runStateUploadPeriod = runStateUploadPeriod;
+
+        this._uploadRunState = this._uploadRunState.bind(this);
+    }
+
+    async start (runId: string) {
+        this._uploadInfo = await this._uploader.getUploadInfo(runId);
+
+        this._uploadRunState();
+
+        this._intervalId = setInterval(this._uploadRunState, this._runStateUploadPeriod);
+
+        return this._uploadInfo.uploadId;
+    }
+
+    async end () {
+        clearInterval(this._intervalId);
+
+        await this._uploadRunState();
+    }
+}
---
+++ test/run-state-uploader.test.ts
@@ -0,0 +1,77 @@
+import assert from 'assert';
+import { RunStateUploader, Uploader, RunStateProvider, } from '../src/run-state-uploader';
+
+describe('Run state uploader tests', () => {
+    let runStateUploader: RunStateUploader | null = null;
+    let uploadedRunState = {};
+    let currentRunState  = {
+        id:     'run_1',
+        passed: 0,
+    };
+
+    const uploader: Uploader = {
+        getUploadInfo: async function getUploadInfo () {
+            return {
+                uploadUrl: 'https://upload.com',
+                uploadId:  'upload_1'
+            };
+        },
+        upload: async function upload (uploadUrl: string, uploadEntity: Buffer) {
+            uploadedRunState = JSON.parse(uploadEntity.toString());
+        }
+    };
+
+    const runStateProvider: RunStateProvider = {
+        getRunState: function getRunState () {
+            return currentRunState;
+        }
+    };
+
+    it('Upload run state on start', async () => {
+        // arrange
+        runStateUploader = new RunStateUploader(uploader, runStateProvider);
+
+        // act
+        const uploadId = await runStateUploader.start('run_1');
+
+        // assert
+        assert.deepStrictEqual(uploadedRunState, currentRunState);
+        assert.deepStrictEqual(uploadId, 'upload_1');
+    });
+
+    it('Upload run state after run state upload period', async () => {
+        // arrange
+        runStateUploader = new RunStateUploader(uploader, runStateProvider, 1000);
+
+        // act
+        await runStateUploader.start('run_1');
+
+        currentRunState = { ...currentRunState, passed: 1 };
+
+        await new Promise((r) => setTimeout(r, 1500));
+
+        // assert
+        assert.deepStrictEqual(uploadedRunState, currentRunState);
+    });
+
+    it('Upload run state after run state on end', async () => {
+        // arrange
+        runStateUploader = new RunStateUploader(uploader, runStateProvider);
+
+        // act
+        await runStateUploader.start('run_1');
+
+        currentRunState = { ...currentRunState, passed: 2 };
+
+        await runStateUploader.end();
+
+        // assert
+        assert.deepStrictEqual(uploadedRunState, currentRunState);
+    });
+
+    afterEach(async () => {
+        await runStateUploader?.end();
+
+        runStateUploader = null;
+    });
+});
