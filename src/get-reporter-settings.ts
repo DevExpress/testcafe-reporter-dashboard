VasilyStrelyaev/testcafe-reporter-dashboard-devextreme

fix an issues with comparer base path

src/get-reporter-settings.ts
@@ -10,8 +10,7 @@ import {
     RESPONSE_TIMEOUT,
     REQUEST_RETRY_COUNT,
     LT_SCREENSHOTS_DIR,
-    LT_DESTINATION_DIR,
-    LT_COMPARER_BASE_DIR
+    LT_DESTINATION_DIR
 } from './env';
 import { ReporterPluginOptions, TaskProperties } from './types';
 import { LayoutTestingSettings } from './types/internal';
@@ -47,7 +46,6 @@ export function getLayoutTestingSettings (taskProperties: TaskProperties): Layou
     return {
         layoutTestingEnabled: LAYOUT_TESTING_ENABLED,
         screenshotsDir:       LT_SCREENSHOTS_DIR || comparerProperties['screenshotsRelativePath'] || '/screenshots',
-        destinationDir:       LT_DESTINATION_DIR || comparerProperties['destinationRelativePath'] || '/artifacts/compared-screenshots',
-        comparerBaseDir:      LT_COMPARER_BASE_DIR || comparerProperties['path'] || './testing'
+        destinationDir:       LT_DESTINATION_DIR || comparerProperties['destinationRelativePath'] || '/artifacts/compared-screenshots'
     };
 }

src/reporter-object-factory.ts
@@ -37,7 +37,7 @@ import createReportUrl from './create-report-url';
 import BLANK_REPORTER from './blank-reporter';
 import path from 'path';
 import { getLayoutTestingSettings } from './get-reporter-settings';
-import { addArrayValueByKey, getScreenshotComparerArtifactsPath, getShouldUploadLayoutTestingData, makePathRelativeStartingWith } from './utils';
+import { addArrayValueByKey, getScreenshotComparerArtifactsPath, getShouldUploadLayoutTestingData } from './utils';
 
 export function reporterObjectFactory (
     readFile: ReadFileMethod,
@@ -235,7 +235,7 @@ export function reporterObjectFactory (
             if (rejectReport) return;
 
             const { screenshots, videos, errs, durationMs, testId, browsers, skipped, unstable, fixture } = testRunInfo;
-            const { layoutTestingEnabled, screenshotsDir, destinationDir, comparerBaseDir }               = layoutTestingSettings;
+            const { layoutTestingEnabled, screenshotsDir, destinationDir }                                = layoutTestingSettings;
 
             const testRunToScreenshotsMap: Record<string, ScreenshotMapItem[]> = {};
 
@@ -278,7 +278,7 @@ export function reporterObjectFactory (
                     if (comparisonFailed) {
                         const testPath                       = fixture.path;
                         const baselineScreenshotPath         = path.join(path.dirname(testPath), 'etalons', path.basename(screenshotPath));
-                        const baselineScreenshotRelativePath = makePathRelativeStartingWith(baselineScreenshotPath, path.normalize(comparerBaseDir));
+                        const baselineScreenshotRelativePath = path.relative(process.cwd(), baselineScreenshotPath);
 
                         if (baselineScreenshotRelativePath) {
                             const posixPath = baselineScreenshotRelativePath.split(path.sep).join(path.posix.sep);

test/reporter-settings.test.ts
@@ -56,15 +56,14 @@ describe('getReporterSettings()', () => {
         });
 
         it('should take env layout options', () => {
-            setLayoutSettingsVariables('true', 'env/screenshot/relative/path', 'env/destination/relative/path', 'env/base/path');
+            setLayoutSettingsVariables('true', 'env/screenshot/relative/path', 'env/destination/relative/path');
             reRequireModules();
 
             const taskProperties = {
                 configuration: {
                     'screenshots-comparer': {
                         screenshotsRelativePath: 'config/screenshot/relative/path',
-                        destinationRelativePath: 'config/destination/relative/path',
-                        path:                    'config/base/path'
+                        destinationRelativePath: 'config/destination/relative/path'
                     }
                 }
             };
@@ -72,8 +71,7 @@ describe('getReporterSettings()', () => {
             const expectedSettings = {
                 layoutTestingEnabled: true,
                 screenshotsDir:       'env/screenshot/relative/path',
-                destinationDir:       'env/destination/relative/path',
-                comparerBaseDir:      'env/base/path'
+                destinationDir:       'env/destination/relative/path'
             };
 
             const actualSettings = getLayoutTestingSettings(taskProperties as any as TaskProperties);
@@ -87,8 +85,7 @@ describe('getReporterSettings()', () => {
                 configuration: {
                     'screenshots-comparer': {
                         screenshotsRelativePath: 'config/screenshot/relative/path',
-                        destinationRelativePath: 'config/destination/relative/path',
-                        path:                    'config/base/path'
+                        destinationRelativePath: 'config/destination/relative/path'
                     }
                 }
             };
@@ -96,8 +93,7 @@ describe('getReporterSettings()', () => {
             const expectedSettings = {
                 layoutTestingEnabled: false,
                 screenshotsDir:       'config/screenshot/relative/path',
-                destinationDir:       'config/destination/relative/path',
-                comparerBaseDir:      'config/base/path'
+                destinationDir:       'config/destination/relative/path'
             };
 
             const actualSettings = getLayoutTestingSettings(taskProperties as any as TaskProperties);
@@ -114,8 +110,7 @@ describe('getReporterSettings()', () => {
             const expectedSettings = {
                 layoutTestingEnabled: false,
                 screenshotsDir:       '/screenshots',
-                destinationDir:       '/artifacts/compared-screenshots',
-                comparerBaseDir:      './testing'
+                destinationDir:       '/artifacts/compared-screenshots'
             };
 
             const actualSettings = getLayoutTestingSettings(taskProperties as any as TaskProperties);

DevExpress/testcafe-reporter-dashboard

src/get-reporter-settings.ts
@@ -10,8 +10,7 @@ import {
     RESPONSE_TIMEOUT,
     REQUEST_RETRY_COUNT,
     LT_SCREENSHOTS_DIR,
-    LT_DESTINATION_DIR,
-    LT_COMPARER_BASE_DIR
+    LT_DESTINATION_DIR
 } from './env';
 import { ReporterPluginOptions, TaskProperties } from './types';
 import { LayoutTestingSettings } from './types/internal';
@@ -47,7 +46,6 @@ export function getLayoutTestingSettings (taskProperties: TaskProperties): Layou
     return {
         layoutTestingEnabled: LAYOUT_TESTING_ENABLED,
         outputRelativeDir:    LT_SCREENSHOTS_DIR || comparerProperties['screenshotsRelativePath'] || '/screenshots',
-        resultsRelativeDir:   LT_DESTINATION_DIR || comparerProperties['destinationRelativePath'] || '/artifacts/compared-screenshots',
-        comparerBaseDir:      LT_COMPARER_BASE_DIR || comparerProperties['path'] || './testing'
+        resultsRelativeDir:   LT_DESTINATION_DIR || comparerProperties['destinationRelativePath'] || '/artifacts/compared-screenshots'
     };
 }

src/reporter-object-factory.ts
@@ -37,7 +37,7 @@ import createReportUrl from './create-report-url';
 import BLANK_REPORTER from './blank-reporter';
 import path from 'path';
 import { getLayoutTestingSettings } from './get-reporter-settings';
-import { addArrayValueByKey, getScreenshotComparerArtifactsPath, getShouldUploadLayoutTestingData, makePathRelativeStartingWith } from './utils';
+import { addArrayValueByKey, getScreenshotComparerArtifactsPath, getShouldUploadLayoutTestingData } from './utils';
 import { S3_OPTIMIZATION_ENABLED } from './env';
 import { s3ReportCommandsFactory } from './s3-report-commands-factory';
 
@@ -235,7 +235,7 @@ export function reporterObjectFactory (
             if (rejectReport) return;
 
             const { screenshots, videos, errs, durationMs, testId, browsers, skipped, unstable, fixture } = testRunInfo;
-            const { layoutTestingEnabled, outputRelativeDir, resultsRelativeDir, comparerBaseDir }        = layoutTestingSettings;
+            const { layoutTestingEnabled, outputRelativeDir, resultsRelativeDir }                         = layoutTestingSettings;
 
             const testRunToScreenshotsMap: Record<string, ScreenshotMapItem[]> = {};
 
@@ -278,7 +278,7 @@ export function reporterObjectFactory (
                     if (comparisonFailed) {
                         const testPath                       = fixture.path;
                         const baselineScreenshotPath         = path.join(path.dirname(testPath), 'etalons', path.basename(screenshotPath));
-                        const baselineScreenshotRelativePath = makePathRelativeStartingWith(baselineScreenshotPath, path.normalize(comparerBaseDir));
+                        const baselineScreenshotRelativePath = path.relative(process.cwd(), baselineScreenshotPath);
 
                         if (baselineScreenshotRelativePath) {
                             const posixPath = baselineScreenshotRelativePath.split(path.sep).join(path.posix.sep);

test/reporter-settings.test.ts
@@ -56,15 +56,14 @@ describe('getReporterSettings()', () => {
         });
 
         it('should take env layout options', () => {
-            setLayoutSettingsVariables('true', 'env/screenshot/relative/path', 'env/destination/relative/path', 'env/base/path');
+            setLayoutSettingsVariables('true', 'env/screenshot/relative/path', 'env/destination/relative/path');
             reRequireModules();
 
             const taskProperties = {
                 configuration: {
                     'screenshots-comparer': {
                         screenshotsRelativePath: 'config/screenshot/relative/path',
-                        destinationRelativePath: 'config/destination/relative/path',
-                        path:                    'config/base/path'
+                        destinationRelativePath: 'config/destination/relative/path'
                     }
                 }
             };
@@ -72,8 +71,7 @@ describe('getReporterSettings()', () => {
             const expectedSettings = {
                 layoutTestingEnabled: true,
                 outputRelativeDir:    'env/screenshot/relative/path',
-                resultsRelativeDir:   'env/destination/relative/path',
-                comparerBaseDir:      'env/base/path'
+                resultsRelativeDir:   'env/destination/relative/path'
             };
 
             const actualSettings = getLayoutTestingSettings(taskProperties as any as TaskProperties);
@@ -87,8 +85,7 @@ describe('getReporterSettings()', () => {
                 configuration: {
                     'screenshots-comparer': {
                         screenshotsRelativePath: 'config/screenshot/relative/path',
-                        destinationRelativePath: 'config/destination/relative/path',
-                        path:                    'config/base/path'
+                        destinationRelativePath: 'config/destination/relative/path'
                     }
                 }
             };
@@ -96,8 +93,7 @@ describe('getReporterSettings()', () => {
             const expectedSettings = {
                 layoutTestingEnabled: false,
                 outputRelativeDir:    'config/screenshot/relative/path',
-                resultsRelativeDir:   'config/destination/relative/path',
-                comparerBaseDir:      'config/base/path'
+                resultsRelativeDir:   'config/destination/relative/path'
             };
 
             const actualSettings = getLayoutTestingSettings(taskProperties as any as TaskProperties);
@@ -114,8 +110,7 @@ describe('getReporterSettings()', () => {
             const expectedSettings = {
                 layoutTestingEnabled: false,
                 outputRelativeDir:    '/screenshots',
-                resultsRelativeDir:   '/artifacts/compared-screenshots',
-                comparerBaseDir:      './testing'
+                resultsRelativeDir:   '/artifacts/compared-screenshots'
             };
 
             const actualSettings = getLayoutTestingSettings(taskProperties as any as TaskProperties);